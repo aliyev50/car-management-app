d313be865051758d45ab303b9b455dbaa78a600c
cameron <aliyevcameron@gmail.com>
2024-07-12 01:29:17 -0400

    build new page and create functionality

--- controllers/cars.js
+++ controllers/cars.js
@@ -11,4 +11,22 @@ router.get('/', async (req, res) => {
       res.redirect('/')
     }
   });
+
+  router.get('/new', async (req, res) => {
+    res.render('cars/new.ejs');
+  });
+
+  router.post('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.cars.push(req.body);
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/cars`);
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+
+
 module.exports = router;
