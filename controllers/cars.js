8bc2fcd8a73c905f863b6c534ee9d2655edb6b61
cameron <aliyevcameron@gmail.com>
2024-07-12 00:52:10 -0400

    build the cars index page

--- controllers/cars.js
+++ controllers/cars.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.get('/', async (req, res) => {
+    try {
+      res.render('cars/index.ejs');
+    } catch (error) {
+      console.log(error)
+      res.redirect('/')
+    }
+  });
 module.exports = router;
